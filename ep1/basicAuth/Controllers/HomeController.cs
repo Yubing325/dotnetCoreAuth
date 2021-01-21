73c066fd3e18c5fdca041a6d489e2e9d68f5ed82
Yubing325 <ybliang000@gmail.com>
2021-01-21 11:21:54 -0600

    add controllers

---
+++ ep1/basicAuth/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ep1.basicAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secrete()
+        {
+            return View();
+        }
+
+        public IActionResult Authenciated()
+        {
+            return RedirectToAction("Index");
+        }
+
+    }
+}
