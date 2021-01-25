ee07a6e135c7f4aa418697ec898cebc8878ae46f
Yubing325 <ybliang000@gmail.com>
2021-01-25 15:42:14 -0600

    add identites

--- ep1/basicAuth/Controllers/HomeController.cs
+++ ep1/basicAuth/Controllers/HomeController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,6 +21,27 @@ namespace ep1.basicAuth.Controllers
 
         public IActionResult Authenticated()
         {
+            //build up the identites
+            var myClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Yubing"),
+                new Claim(ClaimTypes.Email, "Yubing@bingsnote.info"),
+                new Claim("Job", "Engineer")
+            };
+
+            var my2ndClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Yubing L"),
+                new Claim("DriverLicenseYear", "2021")
+            };
+
+            var myIdentity = new ClaimsIdentity(myClaims, "Basic1");
+            var my2ndIdentity = new ClaimsIdentity(my2ndClaims, "Basic2");
+
+            var userPrincipal = new ClaimsPrincipal(new[] {myIdentity, my2ndIdentity});
+            //sign in
+            HttpContext.SignInAsync(userPrincipal);
+
             return RedirectToAction("Index");
         }
 
